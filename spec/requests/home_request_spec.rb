42a66da23657e83810b99b118240470895582020
trashpanda592 <64377283+trashpanda592@users.noreply.github.com>
2021-04-19 00:39:20 -0500

    Create home_request_spec.rb

---
+++ spec/requests/home_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Home", type: :request do    
+      before do
+        @user = create(:user)
+      end
+    
+     describe "When signed in" do
+       before do
+          sign_in @user
+       end
+         
+      it "should get index" do
+        get root_path
+        assert_response :success
+      end
+    end
+
+    describe "when not signed in" do
+      it "should not get index" do
+        get root_path
+        expect(response).to redirect_to user_session_path 
+        #what do we expect should happen if you are not signed in
+      end
+    end
+end
